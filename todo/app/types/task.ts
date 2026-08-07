04e63b447adbe465d261b3dadbb5527543efa042
Rajarshi Roy <rajarshiroy2005456@gmail.com>
2026-08-08 01:07:29 +0530

    final fix

--- todo/app/types/task.ts
+++ todo/app/types/task.ts
@@ -1,7 +1,5 @@
 import type { Prisma } from "@/app/generated/prisma/client";
 
-export type TaskStatus = "pending" | "completed" | "deleted";
-
 export type Task = Prisma.TaskGetPayload<{}>;
 
 export type Tasks = Task[];
