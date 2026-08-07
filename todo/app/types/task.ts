80e327a933ebb1a8275e4ab2852f6dc0ec29003c
Rajarshi Roy <rajarshiroy2005456@gmail.com>
2026-08-08 00:59:32 +0530

    vercel fix 2

--- todo/app/types/task.ts
+++ todo/app/types/task.ts
@@ -1,5 +1,7 @@
 import type { Prisma } from "@/app/generated/prisma/client";
 
+export type TaskStatus = "pending" | "completed" | "deleted";
+
 export type Task = Prisma.TaskGetPayload<{}>;
 
 export type Tasks = Task[];
